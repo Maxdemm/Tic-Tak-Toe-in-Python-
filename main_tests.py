ddeebc36aec19a33aad81eab93471328330cc98f
max <146538140+Maxdemm@users.noreply.github.com>
2023-11-09 22:00:25 +0200

    Add files via upload

---
+++ main_tests.py
@@ -0,0 +1,43 @@
+import unittest
+from unittest.mock import patch
+from main import Game, X_CELL, O_CELL
+
+
+class TestGame(unittest.TestCase):
+    def test_x_wins(self):
+        game = Game()
+
+        x_winning_combination = [1, 2, 3]
+        for a in x_winning_combination:
+            game.set_step(a, True)
+
+        winner = game.check_winning_conditions()
+        self.assertEqual(winner, f'Переможець {X_CELL}')
+        self.assertEqual(game.who_win, None)
+
+    def test_o_wins(self):
+        game = Game()
+
+        o_winning_combination = [1, 5, 9]
+        for a in o_winning_combination:
+            game.set_step(a, False)
+
+        winner = game.check_winning_conditions()
+        self.assertEqual(winner, f'Переможець {O_CELL}')
+        self.assertEqual(game.who_win, None)
+
+    def test_draw(self):
+        game = Game()
+
+        draw_combination = [1, 2, 3, 4, 6, 5, 7, 8, 9]
+        for a in draw_combination:
+            game.set_step(a, game.step_number % 2 == 0)
+
+        self.assertEqual(game.check_winning_conditions(), 'Переможець O')
+        self.assertIsNone(game.winner)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
